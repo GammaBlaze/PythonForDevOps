613f2cde5b482ec342a6f1f910f3e78cc3862936
James Gow <57641234+GammaBlaze@users.noreply.github.com>
2020-03-12 21:39:15 +0000

    Add files via upload

---
+++ Ch1Ex1.py
@@ -0,0 +1,6 @@
+# Write a Python function that takes a name as an argument and prints that name
+def PrintName(name):
+    '''Printing a name passed to the function.'''
+    print(f"Name: {name}")
+
+PrintName("James")
