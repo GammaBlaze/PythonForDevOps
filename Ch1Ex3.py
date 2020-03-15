ac6f04e37420e52988382ecd309b94586a3a1da2
James Gow <57641234+GammaBlaze@users.noreply.github.com>
2020-03-15 21:08:24 +0000

    Add files via upload

---
+++ Ch1Ex3.py
@@ -0,0 +1,4 @@
+# Write a list comprehension that results in a list of every letter in the word smogtether capitalised
+Capitalise = [i.upper() for i in 'smogtether']
+'''No idea why Visual Studio Code requires the print function to display the list, PS & Linux are happy with just Capitalise'''
+print(Capitalise)
